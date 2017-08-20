b486a6646af51073ce3568cf76cc24436d8b96c6
Philip Harrell <alightedlamp@gmail.com>
2017-08-20 09:24:17 -0500

    make some components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,52 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import './App.css';
+import './styles/styles.css';
+
+import SearchBar from './components/SearchBar';
+import StatusBar from './components/StatusBar';
+import Channel from './components/Channel';
 
 class App extends Component {
   constructor() {
     super();
 
+    this.getChannel = this.getChannels.bind(this);
+
     this.state = {
-      // state here
+      channels: [],
+      users: []
+    }
+  }
+  getChannels(channel) {
+    const baseUrl = 'https://wind-bow.gomix.me/twitch-api';
+    let callUrl = '';
+
+    if (Array.isArray(channel)) {
+      for (var i = 0; i < channel.length; i++) {
+        callUrl = `${baseUrl}/channels/${channel[i]}`;
+
+        axios.get(callUrl)
+          .then(response => {
+            console.log(response);
+          });
+      }
     }
   }
   componentDidMount() {
-    // API calls here
+    const defaultChannels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
-    // Update state
+    this.getChannels(defaultChannels);
   }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Twitch TV</h2>
+        <div className="header">
+          <h1 className="title">Twitch TV Stream</h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <SearchBar />
+        <StatusBar />
+        <p className="channel-list">
+          <Channel />
         </p>
       </div>
     );
---
+++ src/components/Channel.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Channel extends React.Component {
+  render() {
+    return(
+      <div className="search-bar">
+        <p>Channel here</p>
+      </div>
+      )
+  }
+}
+
+export default Channel;
